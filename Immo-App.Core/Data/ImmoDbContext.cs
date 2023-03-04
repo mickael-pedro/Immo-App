0b96b6819a49905badff7476ca5940f6e199ca62
Mickael <mickael.pedro@outlook.com>
2023-03-04 19:43:41 +0100

    Created database context ImmoDbContext

---
+++ Immo-App.Core/Data/ImmoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Immo_App.Core.Data
+{
+    public class ImmoDbContext : DbContext
+    {
+        public ImmoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
