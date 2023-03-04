684a57b5605883fcc0e1c559882a9f66ae0054ad
Mickael <mickael.pedro@outlook.com>
2023-03-04 19:24:10 +0100

    Created Tenant Model

---
+++ Immo-App.Core/Models/Tenant.cs
@@ -0,0 +1,12 @@
+﻿namespace Immo_App.Core.Models
+{
+    public class Tenant
+    {
+        public int id { get; set; }
+        public string civility { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string email { get; set; }
+
+    }
+}
