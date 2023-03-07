2f723d58f628abd852933ef580de1ec9770fc47c
Mickael <mickael.pedro@outlook.com>
2023-03-07 19:10:24 +0100

    Added GetFakeTenantList function in TestDataHelper

--- Immo-App.Core.Tests/TestDataHelper.cs
+++ Immo-App.Core.Tests/TestDataHelper.cs
@@ -1,6 +1,28 @@
-﻿namespace Immo_App.Core.Tests
+﻿using Immo_App.Core.Models;
+
+namespace Immo_App.Core.Tests
 {
-    internal class TestDataHelper
+    public class TestDataHelper
     {
+        public static List<Tenant> GetFakeTenantList()
+        {
+            return new List<Tenant>()
+            {
+                new Tenant
+                {
+                    id = 1,
+                    civility = "Monsieur",
+                    first_name = "Jean",
+                    last_name = "Dupont"
+                },
+                new Tenant
+                {
+                    id = 2,
+                    civility = "Madame",
+                    first_name = "Jeanne",
+                    last_name = "Pasquier"
+                }
+            };
+        }
     }
 }
