91325328f3f7f0624743cdee7eccbaac5771517a
Mickael <mickael.pedro@outlook.com>
2023-03-07 20:35:46 +0100

    Get Tenants from Database in Index function of TenantsController

--- Immo-App.Core/Controllers/TenantsController.cs
+++ Immo-App.Core/Controllers/TenantsController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Immo_App.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Immo_App.Core.Controllers
 {
     public class TenantsController : Controller
     {
+        private readonly ImmoDbContext immoDbContext;
+        public TenantsController(ImmoDbContext immoDbContext)
+        {
+            this.immoDbContext = immoDbContext;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return View("Index");
+            var tenants = await immoDbContext.tenant.ToListAsync();
+            return View("Index", tenants);
         }
     }
 }
