296bedad7bad421609c8152decb4cb7512512945
Mickael <mickael.pedro@outlook.com>
2023-03-07 18:33:52 +0100

    Created TenantsController.cs

---
+++ Immo-App.Core/Controllers/TenantsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Immo_App.Core.Controllers
+{
+    public class TenantsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View("Index");
+        }
+    }
+}
