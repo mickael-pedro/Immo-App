907e5f0d047c91e7dd0ea4176cc24ae34ab1daa0
Mickael <mickael.pedro@outlook.com>
2023-03-07 18:30:30 +0100

    Created TenantsControllerIndexTest in TenantsControllerTest.cs

--- Immo-App.Core.Tests/TenantsControllerTest.cs
+++ Immo-App.Core.Tests/TenantsControllerTest.cs
@@ -1,11 +1,22 @@
+using Immo_App.Core.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Immo_App.Core.Tests
 {
     public class TenantsControllerTest
     {
         [Fact]
-        public void Test1()
+        public void TenantsControllerIndexTest()
         {
+            // Arrange
+            var controller = new TenantsController();
 
+            // Act
+            var result = controller.Index();
+            var viewresult = result.Result as ViewResult;
+
+            // Assert
+            Assert.Equal("Index", viewresult.ViewName);
         }
     }
 }
