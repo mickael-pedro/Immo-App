976574514c18104e88121e4d4a30a0b82a37cd6d
Mickael <mickael.pedro@outlook.com>
2023-03-07 19:22:28 +0100

    Assert Tenants count in TenantsControllerIndexTest

--- Immo-App.Core.Tests/TenantsControllerTest.cs
+++ Immo-App.Core.Tests/TenantsControllerTest.cs
@@ -1,5 +1,6 @@
 using Immo_App.Core.Controllers;
 using Immo_App.Core.Data;
+using Immo_App.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Moq.EntityFrameworkCore;
@@ -14,14 +15,16 @@ namespace Immo_App.Core.Tests
             // Arrange
             var dbContextMock = new Mock<ImmoDbContext>();
             dbContextMock.Setup(x => x.tenant).ReturnsDbSet(TestDataHelper.GetFakeTenantList());
-            var controller = new TenantsController();
+            var controller = new TenantsController(dbContextMock.Object);
 
             // Act
             var result = controller.Index();
             var viewresult = result.Result as ViewResult;
+            var model = (List<Tenant>)(viewresult.Model);
 
             // Assert
             Assert.Equal("Index", viewresult.ViewName);
+            Assert.Equal(2, model.Count);
         }
     }
 }
