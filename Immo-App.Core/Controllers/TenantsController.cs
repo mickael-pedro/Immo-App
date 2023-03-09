696d24ea486d3258fd381a317c000e050820a5b2
Mickael <mickael.pedro@outlook.com>
2023-03-09 12:30:33 +0100

    Added Add function in TenantsController

--- Immo-App.Core/Controllers/TenantsController.cs
+++ Immo-App.Core/Controllers/TenantsController.cs
@@ -1,4 +1,5 @@
 ﻿using Immo_App.Core.Data;
+using Immo_App.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,22 @@ namespace Immo_App.Core.Controllers
         {
             var tenants = await immoDbContext.tenant.ToListAsync();
             return View("Index", tenants);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTenantViewModel addTenantRequest)
+        {
+            var tenant = new Tenant()
+            {
+                civility = addTenantRequest.civility,
+                first_name = addTenantRequest.first_name,
+                last_name = addTenantRequest.last_name,
+                email = addTenantRequest.email
+            };
+
+            await immoDbContext.tenant.AddAsync(tenant);
+            await immoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
