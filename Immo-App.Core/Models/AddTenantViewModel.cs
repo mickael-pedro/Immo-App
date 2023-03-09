6a0a2c78be97275ebe7ef712b74933d691705f15
Mickael <mickael.pedro@outlook.com>
2023-03-09 12:29:26 +0100

    Created AddTenantViewModel

---
+++ Immo-App.Core/Models/AddTenantViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Immo_App.Core.Models
+{
+    public class AddTenantViewModel
+    {
+        public string civility { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string email { get; set; }
+    }
+}
