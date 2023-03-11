5295c638180badb14d393bc842247b235b2be453
Mickael <mickael.pedro@outlook.com>
2023-03-11 18:18:08 +0100

    Added TenantsControllerUpdateTest and simplified a line

--- Immo-App.Core.Tests/TenantsControllerTest.cs
+++ Immo-App.Core.Tests/TenantsControllerTest.cs
@@ -42,7 +42,7 @@ namespace Immo_App.Core.Tests
             context.SaveChanges();
 
             var controller = new TenantsController(context);
-            AddTenantViewModel tenantToAdd = new AddTenantViewModel()
+            AddTenantViewModel tenantToAdd = new()
             { civility = "Monsieur", first_name = "Pierre", last_name = "Marchal", email = "p.marchal@example.fr" };
 
             // Act
@@ -56,6 +56,37 @@ namespace Immo_App.Core.Tests
             // Check if ID and email of last added Tenant match what we except
             Assert.Equal(3, tenantAdded.id);
             Assert.Equal("p.marchal@example.fr", tenantAdded.email);
+        }
+
+        [Fact]
+        public void TenantsControllerUpdateTest()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ImmoDbContext>()
+            .UseInMemoryDatabase(databaseName: "immo_db")
+            .Options;
+            var context = new ImmoDbContext(options);
+
+            var tenantFakeList = TestDataHelper.GetFakeTenantList();
+            tenantFakeList.ForEach(t => context.tenant.Add(t));
+            context.SaveChanges();
+
+            var controller = new TenantsController(context);
+            UpdateTenantViewModel tenantToUpdate = new()
+            { id = 1, civility = "Monsieur", first_name = "Paul", last_name = "Dupont", email = "paul@dupont.com" };
+
+            // Act
+            controller.Edit(tenantToUpdate);
+            var result = controller.Index();
+            var viewresult = result.Result as ViewResult;
+            var model = (List<Tenant>)(viewresult.Model);
+
+            // Assert
+            var tenantUpdated = context.tenant.Find(1);
+            // Check if ID, name and email of our edited Tenant match what we except
+            Assert.Equal(1, tenantUpdated.id);
+            Assert.Equal("Paul", tenantUpdated.first_name);
+            Assert.Equal("paul@dupont.com", tenantUpdated.email);
         }
     }
 }
