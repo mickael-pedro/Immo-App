8dc5d6ca416a38f609b97fa77298b00df13a7ddc
Mickael <mickael.pedro@outlook.com>
2023-03-11 18:22:06 +0100

    Created UpdateTenantViewModel

---
+++ Immo-App.Core/Models/UpdateTenantViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Immo_App.Core.Models
+{
+    public class UpdateTenantViewModel
+    {
+        public int id { get; set; }
+        public string civility { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string email { get; set; }
+    }
+}
