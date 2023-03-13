b2ce57dcbace2bbd892202a5ac958facb9e831d5
Mickael <mickael.pedro@outlook.com>
2023-03-13 15:08:24 +0100

    Created ApartmentsController, reusing code from TenantsController

---
+++ Immo-App.Core/Controllers/ApartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Immo_App.Core.Data;
+using Immo_App.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Immo_App.Core.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private readonly ImmoDbContext immoDbContext;
+        public ApartmentsController(ImmoDbContext immoDbContext)
+        {
+            this.immoDbContext = immoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var apartments = await immoDbContext.apartment.OrderBy(a => a.id).ToListAsync();
+            return View("Index", apartments);
+        }
+    }
+}
