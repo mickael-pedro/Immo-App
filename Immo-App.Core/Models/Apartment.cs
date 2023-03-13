3be327dd6e9cfbf6bf726f3e962b70a65ad8c848
Mickael <mickael.pedro@outlook.com>
2023-03-13 14:17:48 +0100

    Created Apartment Model

---
+++ Immo-App.Core/Models/Apartment.cs
@@ -0,0 +1,12 @@
+﻿namespace Immo_App.Core.Models
+{
+    public class Apartment
+    {
+        public int id { get; set; }
+        public string address { get; set; }
+        public string address_complement { get; set; }
+        public string city { get; set; }
+        public int zip_code { get; set; }
+
+    }
+}
