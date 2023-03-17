de81d14947e25cb64a6835c62bc3a4ab4a89ffb3
Mickael <mickael.pedro@outlook.com>
2023-03-17 17:29:07 +0100

    Added GetFakeRentalContractList in TestDataHelper

--- Immo-App.Core.Tests/TestDataHelper.cs
+++ Immo-App.Core.Tests/TestDataHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Immo_App.Core.Models.Apartment;
+using Immo_App.Core.Models.RentalContract;
 using Immo_App.Core.Models.Tenant;
 
 namespace Immo_App.Core.Tests
@@ -47,6 +48,35 @@ namespace Immo_App.Core.Tests
                     address_complement = "ZI Girard",
                     city = "Angers",
                     zip_code = 49100
+                }
+            };
+        }
+
+        public static List<RentalContract> GetFakeRentalContractList()
+        {
+            return new List<RentalContract>()
+            {
+                new RentalContract
+                {
+                    id = 1,
+                    charges_price = 900,
+                    security_deposit_price = 1200,
+                    security_deposit_status = "Non payé",
+                    tenant_balance = 0,
+                    rental_status = "En attente paiement dépot de garantie",
+                    fk_tenant_id = 1,
+                    fk_apartment_id = 1
+                },
+                new RentalContract
+                {
+                    id = 2,
+                    charges_price = 600,
+                    security_deposit_price = 1000,
+                    security_deposit_status = "Payé",
+                    tenant_balance = 0,
+                    rental_status = "En attente état des lieux entrée",
+                    fk_tenant_id = 2,
+                    fk_apartment_id = 2
                 }
             };
         }
