815ddf8aa48108cbd0f45e97f7f838f295158b91
Mickael <mickael.pedro@outlook.com>
2023-03-18 16:10:01 +0100

    Implemented Add function in RentalContractsController

--- Immo-App.Core/Controllers/RentalContractsController.cs
+++ Immo-App.Core/Controllers/RentalContractsController.cs
@@ -25,10 +25,46 @@ namespace Immo_App.Core.Controllers
                                    {
                                        id = rc.id,
                                        tenant_name = t.first_name + " " + t.last_name,
-                                       apartment_address = a.address + ", " + a.zip_code + " " + a.city
+                                       apartment_address = a.address + ", " + a.zip_code + " " + a.city,
+                                       rental_status = rc.rental_status
                                    }).OrderBy(rc => rc.id).ToList();
 
             return View("Index", rentalContracts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var data = new AddRentalContractViewModel
+            {
+                available_apartments = await (from a in immoDbContext.apartment
+                                        where !immoDbContext.rental_contract.Any(r => a.id == r.fk_apartment_id && r.rental_active == true)
+                                        select a).ToListAsync(),
+                tenants = await immoDbContext.tenant.OrderBy(t => t.id).ToListAsync()
+            };
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRentalContractViewModel addRentalContractRequest)
+        {
+            var rentalContract = new RentalContract()
+            {
+                charges_price = addRentalContractRequest.charges_price,
+                rent_price = addRentalContractRequest.rent_price,
+                security_deposit_price = addRentalContractRequest.security_deposit_price,
+                security_deposit_status = "Non payé",
+                tenant_balance = 0,
+                rental_status = "En attente du paiement du dépôt de garantie",
+                rental_active = true,
+                fk_tenant_id = addRentalContractRequest.fk_tenant_id,
+                fk_apartment_id = addRentalContractRequest.fk_apartment_id
+            };
+
+            await immoDbContext.rental_contract.AddAsync(rentalContract);
+            await immoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Immo-App.Core/Models/RentalContract/AddRentalContractViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Immo_App.Core.Models.RentalContract
+{
+    public class AddRentalContractViewModel
+    {
+        public float charges_price { get; set; }
+        public float rent_price { get; set; }
+        public float security_deposit_price { get; set; }
+        public int fk_tenant_id { get; set; }
+        public int fk_apartment_id { get; set; }
+        public List<Apartment.Apartment> available_apartments { get; set; }
+        public List<Tenant.Tenant> tenants { get; set; }
+    }
+}
