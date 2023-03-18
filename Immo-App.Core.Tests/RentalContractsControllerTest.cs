aa0732231fd8d75c42be1ecb11695cead45b2ed0
Mickael <mickael.pedro@outlook.com>
2023-03-18 17:50:37 +0100

    Implemented RentalContractsControllerEditTest

--- Immo-App.Core.Tests/RentalContractsControllerTest.cs
+++ Immo-App.Core.Tests/RentalContractsControllerTest.cs
@@ -67,7 +67,7 @@ namespace Immo_App.Core.Tests
 
             // Assert
             var rentalContractAdded = context.rental_contract.Find(1);
-            // Assert that tenant's name and apartment's address tied to the rental contract match what we except
+            // Assert that the information of the rental contract added match what we except
             Assert.Equal(600, rentalContractAdded.charges_price);
             Assert.Equal(800, rentalContractAdded.rent_price);
             Assert.Equal("Non payé", rentalContractAdded.security_deposit_status);
@@ -75,6 +75,55 @@ namespace Immo_App.Core.Tests
             Assert.Equal("En attente du paiement du dépôt de garantie", rentalContractAdded.rental_status);
             Assert.Equal(true, rentalContractAdded.rental_active);
             Assert.Equal(1, rentalContractAdded.fk_tenant_id);
+        }
+
+        [Fact]
+        public void RentalContractsControllerEditTest()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ImmoDbContext>()
+            .UseInMemoryDatabase(databaseName: "immo_db")
+            .Options;
+            var context = new ImmoDbContext(options);
+            context.Database.EnsureDeleted();
+
+            var apartmentFakeList = TestDataHelper.GetFakeApartmentList();
+            apartmentFakeList.ForEach(a => context.apartment.Add(a));
+            var tenantFakeList = TestDataHelper.GetFakeTenantList();
+            tenantFakeList.ForEach(t => context.tenant.Add(t));
+            var rentalContractFakeList = TestDataHelper.GetFakeRentalContractList();
+            rentalContractFakeList.ForEach(r => context.rental_contract.Add(r));
+            context.SaveChanges();
+
+            var controller = new RentalContractsController(context);
+            var rentalContractBeforeEdit = context.rental_contract.Find(2);
+            RentalContract rentalContractToEdit = new()
+            {
+                id = rentalContractBeforeEdit.id,
+                charges_price = 700,
+                rent_price = 900,
+                security_deposit_price = rentalContractBeforeEdit.security_deposit_price,
+                security_deposit_status = rentalContractBeforeEdit.security_deposit_status,
+                tenant_balance = rentalContractBeforeEdit.tenant_balance,
+                rental_status = rentalContractBeforeEdit.rental_status,
+                fk_tenant_id = rentalContractBeforeEdit.fk_tenant_id,
+                fk_apartment_id = rentalContractBeforeEdit.fk_apartment_id
+            };
+
+            // Act
+            controller.Edit(rentalContractToEdit);
+
+            // Assert
+            var rentalContractEdited = context.rental_contract.Find(2);
+            // Assert that the updated information match what we except along with the information that shouldn't be changed 
+            Assert.Equal(700, rentalContractEdited.charges_price);
+            Assert.Equal(900, rentalContractEdited.rent_price);
+            Assert.Equal("Payé", rentalContractEdited.security_deposit_status);
+            Assert.Equal(200, rentalContractEdited.tenant_balance);
+            Assert.Equal("En attente état des lieux entrée", rentalContractEdited.rental_status);
+            Assert.Equal(true, rentalContractEdited.rental_active);
+            Assert.Equal(2, rentalContractEdited.fk_tenant_id);
+            Assert.Equal(2, rentalContractEdited.fk_apartment_id);
         }
     }
 }
