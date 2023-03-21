7ed5739724a850eb3a9ca70be7db065f1712f4bb
Mickael <mickael.pedro@outlook.com>
2023-03-21 12:35:17 +0100

    Made address_complement nullable in Apartment model

--- Immo-App.Core/Models/Apartment/Apartment.cs
+++ Immo-App.Core/Models/Apartment/Apartment.cs
@@ -4,7 +4,7 @@
     {
         public int id { get; set; }
         public string address { get; set; }
-        public string address_complement { get; set; }
+        public string? address_complement { get; set; }
         public string city { get; set; }
         public int zip_code { get; set; }
 
