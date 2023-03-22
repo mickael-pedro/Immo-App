56c4d055776327c423ea70ee9503bc4277fb4874
Mickael <mickael.pedro@outlook.com>
2023-03-22 21:45:43 +0100

    Created InventoryFixture model

---
+++ Immo-App.Core/Models/InventoryFixture/InventoryFixture.cs
@@ -0,0 +1,11 @@
+﻿namespace Immo_App.Core.Models.InventoryFixture
+{
+	public class InventoryFixture
+    {
+		public int id { get; set; }
+		public DateTime date_inv { get; set; }
+		public string type { get; set; }
+		public string notes { get; set; }
+		public int fk_rental_contract_id { get; set; }
+	}
+}
