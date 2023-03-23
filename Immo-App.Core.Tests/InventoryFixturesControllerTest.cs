d3d14e03dc6a86fb4ef4f154fd5b70bfe422e46a
Mickael <mickael.pedro@outlook.com>
2023-03-23 11:19:37 +0100

    Implemented InventoryFixturesControllerEditTest

--- Immo-App.Core.Tests/InventoryFixturesControllerTest.cs
+++ Immo-App.Core.Tests/InventoryFixturesControllerTest.cs
@@ -34,11 +34,48 @@ namespace Immo_App.Core.Tests
 
             // Assert
             var inventoryFixtureAdded = context.inventory_fixture.Find(1);
-            // Assert that the information of the rental contract added match what we except
+            // Assert that the information of the inventory fixture added match what we except
             Assert.Equal(DateTime.Parse("2023-02-04").ToUniversalTime(), inventoryFixtureAdded.date_inv);
             Assert.Equal("Entrée", inventoryFixtureAdded.type);
             Assert.Equal("Test notes", inventoryFixtureAdded.notes);
             Assert.Equal(1, inventoryFixtureAdded.fk_rental_contract_id);
+        }
+
+        [Fact]
+        public void InventoryFixturesControllerEditTest()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ImmoDbContext>()
+            .UseInMemoryDatabase(databaseName: "immo_db")
+            .Options;
+            var context = new ImmoDbContext(options);
+            context.Database.EnsureDeleted();
+
+            var apartmentFakeList = TestDataHelper.GetFakeApartmentList();
+            apartmentFakeList.ForEach(a => context.apartment.Add(a));
+            var tenantFakeList = TestDataHelper.GetFakeTenantList();
+            tenantFakeList.ForEach(t => context.tenant.Add(t));
+            var rentalContractFakeList = TestDataHelper.GetFakeRentalContractList();
+            rentalContractFakeList.ForEach(r => context.rental_contract.Add(r));
+            var inventoryFixtureFakeList = TestDataHelper.GetFakeInventoryFixtureList();
+            inventoryFixtureFakeList.ForEach(i => context.inventory_fixture.Add(i));
+            context.SaveChanges();
+
+            var controller = new InventoryFixturesController(context);
+            var inventoryFixtureBeforeEdit = context.inventory_fixture.Find(1);
+            InventoryFixture inventoryFixtureToEdit = new()
+            { id = inventoryFixtureBeforeEdit.id, date_inv = DateTime.Parse("2023-02-04"), type = "Entrée", notes = "Test Edit", fk_rental_contract_id = 1 };
+
+            // Act
+            controller.Edit(inventoryFixtureToEdit);
+
+            // Assert
+            var inventoryFixtureAdded = context.inventory_fixture.Find(1);
+            // Assert that the information of the inventory fixture edited match what we except
+            Assert.Equal(DateTime.Parse("2023-02-04").ToUniversalTime(), inventoryFixtureAdded.date_inv);
+            Assert.Equal(inventoryFixtureBeforeEdit.type, inventoryFixtureAdded.type);
+            Assert.Equal("Test Edit", inventoryFixtureAdded.notes);
+            Assert.Equal(inventoryFixtureBeforeEdit.fk_rental_contract_id, inventoryFixtureAdded.fk_rental_contract_id);
         }
     }
 }
