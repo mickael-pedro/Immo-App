d169bd272b10ca7cc72074fe913652feb1ee3ac8
Mickael <mickael.pedro@outlook.com>
2023-03-25 19:00:52 +0100

    Created AddInvoiceViewModel model

---
+++ Immo-App.Core/Models/Invoice/AddInvoiceViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Immo_App.Core.Models.Invoice
+{
+	public class AddInvoiceViewModel
+    {
+		public DateTime date_invoice { get; set; }
+		public float amount { get; set; }
+		public string type { get; set; }
+		public int fk_rental_contract_id { get; set; }
+	}
+}
