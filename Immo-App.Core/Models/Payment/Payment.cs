a6178012857c468e0500236e7075ce7e625f4f79
Mickael <mickael.pedro@outlook.com>
2023-03-27 12:31:13 +0200

    Created Payment model

---
+++ Immo-App.Core/Models/Payment/Payment.cs
@@ -0,0 +1,12 @@
+﻿namespace Immo_App.Core.Models.Payment
+{
+    public class Payment
+    {
+        public int id { get; set; }
+        public DateTime date_payment { get; set; }
+        public float amount { get; set; }
+        public string origin { get; set; }
+        public int fk_invoice_id { get; set; }
+        public int fk_rental_contract_id { get; set; }
+    }
+}
