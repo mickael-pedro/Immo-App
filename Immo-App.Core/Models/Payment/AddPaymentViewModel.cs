b82c65e7476ddec71ab82212ccab03786af31dfb
Mickael <mickael.pedro@outlook.com>
2023-03-27 12:37:12 +0200

    Created AddPaymentViewModel model

---
+++ Immo-App.Core/Models/Payment/AddPaymentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Immo_App.Core.Models.Payment
+{
+    public class AddPaymentViewModel
+    {
+        public DateTime date_payment { get; set; }
+        public float amount { get; set; }
+        public string origin { get; set; }
+        public int fk_invoice_id { get; set; }
+        public int fk_rental_contract_id { get; set; }
+        public List<Invoice.Invoice> invoices { get; set; }
+    }
+}
