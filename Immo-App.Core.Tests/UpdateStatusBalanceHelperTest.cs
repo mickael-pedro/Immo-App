95aef851e483c1040d5ddf2dc5d5c3920b31a94c
Mickael <mickael.pedro@outlook.com>
2023-03-29 18:47:14 +0200

    Implemented UpdateRentalStatusTest and updated database name

--- Immo-App.Core.Tests/UpdateStatusBalanceHelperTest.cs
+++ Immo-App.Core.Tests/UpdateStatusBalanceHelperTest.cs
@@ -11,7 +11,7 @@ namespace Immo_App.Core.Tests
         {
             // Arrange
             var options = new DbContextOptionsBuilder<ImmoDbContext>()
-            .UseInMemoryDatabase(databaseName: "immo_db")
+            .UseInMemoryDatabase(databaseName: "immo_db_status_balance")
             .Options;
             var context = new ImmoDbContext(options);
             context.Database.EnsureDeleted();
@@ -38,6 +38,42 @@ namespace Immo_App.Core.Tests
             var rentalContract = context.rental_contract.Find(1);
             // Assert that the balance has been correctly updated (Invoice is 350 and payment is 300)
             Assert.Equal(-50, rentalContract.tenant_balance);
+        }
+
+        [Fact]
+        public void UpdateRentalStatusTest()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ImmoDbContext>()
+            .UseInMemoryDatabase(databaseName: "immo_db_status_balance")
+            .Options;
+            var context = new ImmoDbContext(options);
+            context.Database.EnsureDeleted();
+
+            var apartmentFakeList = TestDataHelper.GetFakeApartmentList();
+            apartmentFakeList.ForEach(a => context.apartment.Add(a));
+            var tenantFakeList = TestDataHelper.GetFakeTenantList();
+            tenantFakeList.ForEach(t => context.tenant.Add(t));
+            var rentalContractFakeList = TestDataHelper.GetFakeRentalContractList();
+            rentalContractFakeList.ForEach(r => context.rental_contract.Add(r));
+            var inventoryFixtureFakeList = TestDataHelper.GetFakeInventoryFixtureList();
+            inventoryFixtureFakeList.ForEach(i => context.inventory_fixture.Add(i));
+            var invoiceFakeList = TestDataHelper.GetFakeInvoiceList();
+            invoiceFakeList.ForEach(i => context.invoice.Add(i));
+            var paymentFakeList = TestDataHelper.GetFakePaymentList();
+            paymentFakeList.ForEach(i => context.payment.Add(i));
+            context.SaveChanges();
+
+            var helper = new UpdateStatusBalanceHelper(context);
+
+            // Act
+            // Status of rental contract is "En attente état des lieux entrée" before the update
+            helper.UpdateRentalStatus(2);
+
+            // Assert
+            var rentalContract = context.rental_contract.Find(2);
+            // Assert that the Status has been correctly updated
+            Assert.Equal("En cours", rentalContract.rental_status);
         }
     }
 }
