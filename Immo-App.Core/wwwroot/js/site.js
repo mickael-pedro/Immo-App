79d16dfe2f5c62a1f2585a47aee0544326ba1904
Mickael <mickael.pedro@outlook.com>
2023-04-03 17:52:17 +0200

    Only allow decimals and numbers for monetary input fields

--- Immo-App.Core/wwwroot/js/site.js
+++ Immo-App.Core/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode != 46 && charCode != 44 && (charCode < 48 || charCode > 57)))
+        return false;
+    return true;
+}
